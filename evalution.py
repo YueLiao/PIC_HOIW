11182843cb5e6c55f749759ffea0493dbecbb9b9
Yue Liao <liaoyue.ai@gmail.com>
2019-10-09 13:39:01 +0800

    fix iou bug

--- evalution.py
+++ evalution.py
@@ -142,17 +142,17 @@ class hoiw_eval():
             rec1 = bbox1['bbox']
             rec2 = bbox2['bbox']
             # computing area of each rectangles
-            S_rec1 = rec1[2] * rec1[3]
-            S_rec2 = rec2[2] * rec2[3]
+            S_rec1 = (rec1[2] - rec1[0]) * (rec1[3] - rec1[1])
+            S_rec2 = (rec2[2] - rec2[0]) * (rec2[3] - rec2[1])
 
             # computing the sum_area
             sum_area = S_rec1 + S_rec2
 
             # find the each edge of intersect rectangle
             left_line = max(rec1[1], rec2[1])
-            right_line = min(rec1[3] + rec1[1], rec2[3] + rec2[1])
+            right_line = min(rec1[3], rec2[3])
             top_line = max(rec1[0], rec2[0])
-            bottom_line = min(rec1[2] + rec1[0], rec2[2] + rec1[0])
+            bottom_line = min(rec1[2], rec2[2])
             # judge if there is an intersect
             if left_line >= right_line or top_line >= bottom_line:
                 return 0
